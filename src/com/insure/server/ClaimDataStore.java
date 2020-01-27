b9fd4ebcbe199bca361b9b7a1a8870da38e28381
josecsousa <carlossousa93@hotmail.com>
2020-01-27 16:15:26 +0000

    Claim DataStore

--- src/com/insure/server/ClaimDataStore.java
+++ src/com/insure/server/ClaimDataStore.java
@@ -1,5 +1,24 @@
 package com.insure.server;
 
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ClaimDataStore {
+    // field to keep a Unique ID
+    private final AtomicInteger ID = new AtomicInteger(0);
+    private String Description;
+    // field with a Collection to store the data
+    private Map<Integer, String> DataStore;
+
+    // Constructor also create initial data
+
+    public ClaimDataStore(){
+    // preload data (create a few claims)
+    }
+
+    // define and implement the API:
+    // create/retrieve/update claims on the data store safely, ex:
+    // int createClaim() { //return a unique claim id }
+    // list/create/read/update/delete documents of claims on the datastore safely.
 
 }
