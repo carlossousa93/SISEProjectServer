c2979b594bcbc4e29885ed01b0a7f375757718f6
josecsousa <carlossousa93@hotmail.com>
2020-01-27 15:29:26 +0000

    Acrescentado a metodos à Claim

--- src/com/insure/server/Claim.java
+++ src/com/insure/server/Claim.java
@@ -1,4 +1,27 @@
 package com.insure.server;
 
 public class Claim {
+    private int uuid;
+    private String description;
+    public Claim (int uuid, String description){
+        this.uuid = uuid;
+        this.description = description;
+    }
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+    public int getUuid() {
+        return uuid;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    @Override
+    public String toString(){
+        return "Claim ID:" + uuid + "\n" +
+                "Description:" + this.description;
+    }
 }
--- src/com/insure/server/ClaimDataStore.java
+++ src/com/insure/server/ClaimDataStore.java
@@ -1,4 +1,5 @@
 package com.insure.server;
 
 public class ClaimDataStore {
+
 }
