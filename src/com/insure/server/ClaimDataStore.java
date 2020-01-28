615dd2e95ea7f54f665e12d55c149d777d782d64
josecsousa <carlossousa93@hotmail.com>
2020-01-28 17:23:59 +0000

    Claim DataStore

--- src/com/insure/server/ClaimDataStore.java
+++ src/com/insure/server/ClaimDataStore.java
@@ -28,8 +28,10 @@ public class ClaimDataStore {
     // Create Claim: Da parte do cliente, ele apenas conseguirá utilizar este metodo para criar uma claim com descrição.
     // O ID é criado automaticamente do lado do Server
     public int createClaim(String Description, int userID){
-        Claim claim = new Claim(ID.incrementAndGet(), Description, userID);
-        return ID.get();
+        Claim claim = new Claim(ID.get(), Description, userID);
+        int claim_ID = claim.getUuid();
+        ID.incrementAndGet();
+        return claim_ID;
         // criar metodo de verificação ID do cliente/funcionário
     }
 
@@ -37,39 +39,4 @@ public class ClaimDataStore {
     // int createClaim() { //return a unique claim id }
     // list/create/read/update/delete documents of claims on the datastore safely.
 
-
-    static class MyThread extends Thread {
-
-        //private ClaimDataStore DB;
-        ClaimDataStore DB = new ClaimDataStore();
-        private String Description;
-        private int userID;
-
-        // update the constructor to use a counter
-        MyThread(String Description, int userID) {
-            this.Description = Description;
-            this.userID = userID;
-        }
-
-        @Override
-        public void run() {
-            // update the loop
-            for (int i = 0; i < 6; i++) {
-                // writeHello();
-                int claim = DB.createClaim(Description, userID);
-                System.out.println(Description + " " + claim);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        // fix the calls to the updated constructor
-        Thread a = new MyThread("a",1);
-        Thread b = new MyThread("B", 2);
-        a.start();
-        b.start();
-        a.join();
-        b.join();
-        // output the final counter value
-    }
 }
--- src/com/insure/server/Main.java
+++ src/com/insure/server/Main.java
@@ -1,7 +1,40 @@
 package com.insure.server;
 
 public class Main {
-    public static void main(String args[]){
-        System.out.println("Project template - server");
+    static class MyThread extends Thread {
+
+        //private ClaimDataStore DB;
+        private ClaimDataStore DS;
+        private String Description;
+        private int userID;
+
+        // update the constructor to use a counter
+        MyThread(String Description, int userID) {
+            this.Description = Description;
+            this.userID = userID;
+        }
+
+        @Override
+        public void run() {
+            // update the loop
+            for (int i = 0; i < 6; i++) {
+                // writeHello();
+                int claim = DS.createClaim(Description, userID);
+                System.out.println(Description + " " + claim);
+            }
+        }
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        //System.out.println("Project template - server");
+
+        // fix the calls to the updated constructor
+        Thread a = new MyThread("a",1);
+        Thread b = new MyThread("B", 2);
+        a.start();
+        b.start();
+        a.join();
+        b.join();
+        // output the final counter value
     }
 }
