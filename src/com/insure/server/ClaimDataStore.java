40a80ab2738ae7ccd8dbc9dd86bbb0d4bf20e144
josecsousa <carlossousa93@hotmail.com>
2020-01-28 17:30:12 +0000

    Claim DataStore

--- src/com/insure/server/ClaimDataStore.java
+++ src/com/insure/server/ClaimDataStore.java
@@ -39,4 +39,40 @@ public class ClaimDataStore {
     // int createClaim() { //return a unique claim id }
     // list/create/read/update/delete documents of claims on the datastore safely.
 
+    static class MyThread extends Thread {
+
+        //private ClaimDataStore DB;
+        ClaimDataStore DS = new ClaimDataStore();
+        private String Description;
+        private int userID;
+
+        // update the constructor to use a counter
+        MyThread(String Description, int userID) {
+            this.Description = Description;
+            this.userID = userID;
+        }
+
+        @Override
+        public void run() {
+            // update the loop
+            for (int i = 0; i < 6; i++) {
+                int claim = DS.createClaim(Description, userID);
+                System.out.println(Description + " " + claim);
+            }
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        //System.out.println("Project template - server");
+
+        // fix the calls to the updated constructor
+        Thread a = new Main.MyThread("a",1);
+        Thread b = new Main.MyThread("B", 2);
+        a.start();
+        b.start();
+        a.join();
+        b.join();
+        // output the final counter value
+    }
+
 }
--- src/com/insure/server/Main.java
+++ src/com/insure/server/Main.java
@@ -1,39 +1,9 @@
 package com.insure.server;
 
 public class Main {
-    static class MyThread extends Thread {
 
-        //private ClaimDataStore DB;
-        ClaimDataStore DS = new ClaimDataStore();
-        private String Description;
-        private int userID;
+    public static void main(String args[]){
+        System.out.println("Project template - server");
 
-        // update the constructor to use a counter
-        MyThread(String Description, int userID) {
-            this.Description = Description;
-            this.userID = userID;
-        }
-
-        @Override
-        public void run() {
-            // update the loop
-            for (int i = 0; i < 6; i++) {
-                int claim = DS.createClaim(Description, userID);
-                System.out.println(Description + " " + claim);
-            }
-        }
-    }
-
-    public static void main(String args[]) throws Exception {
-        //System.out.println("Project template - server");
-
-        // fix the calls to the updated constructor
-        Thread a = new MyThread("a",1);
-        Thread b = new MyThread("B", 2);
-        a.start();
-        b.start();
-        a.join();
-        b.join();
-        // output the final counter value
     }
 }
