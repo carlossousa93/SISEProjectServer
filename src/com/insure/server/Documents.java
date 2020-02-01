d684e6379bee8926e28e999e3e7634c34f99688a
josecsousa <carlossousa93@hotmail.com>
2020-02-01 16:19:11 +0000

    Claim DataStore

--- src/com/insure/server/Documents.java
+++ src/com/insure/server/Documents.java
@@ -1,4 +1,15 @@
 package com.insure.server;
 
 public class Documents {
+
+    String content;
+
+    public Documents(String content){
+        this.content = content;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
 }
