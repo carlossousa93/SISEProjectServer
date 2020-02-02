defb5d3a3bdd4e961a265f66ccbbfacc5cdf30a2
josecsousa <carlossousa93@hotmail.com>
2020-02-02 12:56:05 +0000

    Claim DataStore

--- src/com/insure/server/Claim.java
+++ src/com/insure/server/Claim.java
@@ -1,36 +1,50 @@
 package com.insure.server;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Claim {
-    private final int id; // id da claim
+    private AtomicInteger claimID = new AtomicInteger(0);
+    private AtomicInteger docID = new AtomicInteger(0);
+
     private String description;
-    private final int userID; //
+    public String UserID;
+    public Map<Integer, Document> documents = new ConcurrentHashMap<Integer, Document>();
 
-    public Claim (int id, String description, int userID){
-        this.id = id;
+    // Constructor of the claim
+    public Claim(AtomicInteger ClaimID, String description, String UserID) {
+        this.claimID = claimID;
         this.description = description;
-        this.userID = userID;
+        this.UserID = UserID;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getUserID() {
-        return userID;
-    }
-
-    public void setDescription(String description) {
+    //Writes description of claim
+    public void setClaimDescription(String description) {
         this.description = description;
+    }
+
+    public int getClaimID(){
+        return this.claimID.get();
+    }
+
+    public String getUserID(){
+        return this.UserID;
+    }
+
+    public void addDocument(Document document){
+        int docID = document.getdocID();
+
+        this.docID = i;
+        documents.put(i, document);
+    }
+
+    public void increment() {
+        this.claimID.incrementAndGet();
     }
 
     @Override
     public String toString(){
-        return "Claim ID: " + id  + " Description:" + this.description;
+        return "Claim ID: " + claimID  + " Description:" + this.description;
     }
 }
--- src/com/insure/server/ClaimDataStore.java
+++ src/com/insure/server/ClaimDataStore.java
@@ -1,34 +1,76 @@
 package com.insure.server;
 
-        import com.sun.org.apache.bcel.internal.generic.NEW;
-
-        import javax.jws.WebService;
-        import java.util.Map;
-        import java.util.concurrent.ConcurrentHashMap;
-        import java.util.concurrent.atomic.AtomicInteger;
+import javax.jws.WebService;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @WebService
 public class ClaimDataStore {
-    // field to keep a Unique ID
-    public AtomicInteger ID = new AtomicInteger(0);
-    public String Description;
-    public int userID;
-    // field with a Collection to store the data
-    protected Map<Integer, String> DataStore  = new ConcurrentHashMap<Integer, String>();
+    public Map<Integer, Claim> Datastore = new ConcurrentHashMap<Integer, Claim>(); //claimID, Claim
+    public Set<String> UsersStore = new HashSet<String>();
 
-    // Constructor also create initial data
-    public ClaimDataStore(){
-        //this.DataStore = new ConcurrentHashMap<Integer, String>();
-        //this.ID = new AtomicInteger(0);
-        // preload data (create a few claims)
+    public AtomicInteger ClaimID = new AtomicInteger(1);
+    public String description;
+    public String UserID;
+
+    //Constructor
+    public ClaimDataStore() {
     }
-    // define and implement the API:
-    // Create Claim: Da parte do cliente, ele apenas conseguirá utilizar este metodo para criar uma claim com descrição.
-    // O ID é criado automaticamente do lado do Server
-    public int createClaim(String Description, int userID){
-        Claim claim = new Claim(ID.incrementAndGet(), Description, userID);
-        return claim.getId();
-        // criar metodo de verificação ID do cliente/funcionário
+
+    //Method adds Claim to the datastore
+    public int addClaim(String description, String UserID) {
+        if (!UsersIDVerify(UserID)) {
+            System.out.println("You are not our client");
+        } else {
+            Claim claim = createClaim(description, UserID);
+            Datastore.put(claim.getClaimID(), claim);
+            return claim.getClaimID();
+        }
+        return 0;
+    }
+
+    // Method create new claim
+    public Claim createClaim(String description, String UserID){
+        Claim claim = new Claim(ClaimID, description, UserID);
+        return claim;
+    }
+
+    //checks if a claim is in the DataStore
+    public boolean isClaimByID(int claimID) {
+        return Datastore.containsKey(claimID);
+    }
+
+    // Verify client is insured
+    public boolean UsersIDVerify(String UserID){
+        return UsersStore.contains(UserID);
+    }
+
+    //Verifies if client owns target claim
+    public boolean clientToClaim(String UserID, int claimID){
+        return Datastore.get(claimID).getUserID().equals(UserID);
+    }
+
+    //Associates & creates document to a claim, checks if the claim exists, if the person signing is employee/client, receives the content of the document
+    public void associateDocument(int claimID, String UserID,String content) {
+        if ((UsersIDVerify(UserID) && isClaimByID(claimID) && clientToClaim(UserID, claimID))) {
+            String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()); // Find how to return current timestamp
+            Document document = new Document(content, timestamp);
+            Datastore.get(claimID).addDocument(document);
+        }
+        else{
+            System.out.println("You can't add Documents to this claim/ Claim doesn't exit");
+        }
+    }
+
+    //Checks if claim exists and prints out contents for each document associated with it
+    public void listDocument(int claimID) {
+        if (!isClaimByID(claimID)) {
+            System.out.println("This claim doesn't exist");
+        } else {
+            Datastore.get(claimID).documents.forEach((K,V) -> System.out.println("DocID:" + K + "\n Content:" + V.getContents() + "\n ----------------- \n\n" ));
+        }
     }
 
     // create/retrieve/update claims on the data store safely, ex:
